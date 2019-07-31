e89ad74ba4be8e5335c55c6f9bae90f825dd19d8
Ubuntu <ubuntu@ip-172-31-25-22.ec2.internal>
2019-07-31 04:24:29 +0000

    files

--- java_sockets/FileSplit.java
+++ java_sockets/FileSplit.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
 import java.net.*;
+import org.apache.commons.io.IOUtils;
 
 public class FileSplit {
 
@@ -15,8 +16,9 @@ public class FileSplit {
     	InputStream inputStream = null;
     	try 
    	{
-        	inputStream = new FileInputStream(input_path + "terms.pdf");
-    	    	byte[] buffer = new byte[8192];
+		int blocksize = 8192;
+        	inputStream = new FileInputStream(input_path + "voda.pdf");
+    	    	byte[] buffer = new byte[blocksize];
     		ByteArrayOutputStream baos = new ByteArrayOutputStream();
      		ByteArrayOutputStream ba = new ByteArrayOutputStream();
 
@@ -27,7 +29,7 @@ public class FileSplit {
       			baos.write(buffer, 0, bytesRead);
 			i++;
     		}
-       		byte[][] allbyte=new byte[i][8192];
+       		byte[][] allbyte=new byte[i][blocksize];
 
 		byte b [] = baos.toByteArray();
 		// b.length array length
@@ -36,16 +38,21 @@ public class FileSplit {
                 	System.out.print((char)b[x]);
                 }
 		for(int x = 0; x < i; x++) {
-			int byteblock = 8192*x;
-			int endblock = byteblock + 8192;
+			int byteblock = blocksize*x;
+			int endblock = byteblock + blocksize;
 			if (endblock > b.length) endblock = b.length; // last block
 			allbyte[x] = Arrays.copyOfRange(b,byteblock,endblock);
 		}
 
+
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
 
     		for(int x=0;x<i;x++) {
+		        OutputStream ost = new FileOutputStream("./voda" + x + ".pdf");
+
 			outputStream.write( allbyte[x] );
+			ost.write(allbyte[x]);
+			ost.close();
 		}
 
 		byte c[] = outputStream.toByteArray( );
@@ -53,17 +60,19 @@ public class FileSplit {
 
 		DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
 
-		dOut.writeInt(c.length); // write length of the message
-		dOut.write(c); 
+//		dOut.writeInt(allbyte.length); // write length of the message
+		dOut.write(allbyte[0]); 
+// 		dOut.write("testing\n");
+		//dOut.close();
 
-		OutputStream os = new FileOutputStream("./termsout.pdf");
+		OutputStream os = new FileOutputStream("./vodanew.pdf");
 
                 // Starts writing the bytes in it
                 os.write(b);
 
                 // Close the file
                 os.close();
-  		OutputStream os1 = new FileOutputStream("./termsout1.pdf");
+  		OutputStream os1 = new FileOutputStream("./vodanew1.pdf");
 
                 // Starts writing the bytes in it
                 os1.write(c);
@@ -71,7 +80,18 @@ public class FileSplit {
 
                 // Close the file
                 os1.close();
+		System.out.println("waiting for answer");
 
+//	 	Socket client = new Socket(serverName, port);
+
+     		InputStream inFromServer1 = socket.getInputStream();
+         	DataInputStream in1 = new DataInputStream(inFromServer1);
+		//byte[] inbytes = IOUtils.toByteArray(in1);
+		String strIn = IOUtils.toString(in1);
+         	System.out.println("Server says " + in1);
+         	socket.close();
+		System.out.println("closing");
+		dOut.close();
 
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
